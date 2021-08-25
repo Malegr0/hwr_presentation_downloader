cafe22ef3ce4648e2c30f9815423e3c6ed7d67ce
Malegr0 <schoening.andreas@gmx.de>
2021-08-25 16:08:47 +0200

    added file and utf-8 encoding

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
