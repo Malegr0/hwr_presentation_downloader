13c73b599e121b9e8b3987ca394d15c9723e5dad
Malegr0 <schoening.andreas@gmx.de>
2021-08-25 20:02:55 +0200

    changed to pdfkit using wkhtmltopdf

--- crawler.py
+++ crawler.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-
+import imgkit
 import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+import pdfkit
 
 
-def get_html_from_url(website_url: str) -> str:
+
+def get_pdf_from_url(website_url: str) -> str:
     """
     Method to receive the html text of an url.
 
     :param website_url: String of the url which will be searched for
     :return: complete html text as string
     """
-    html_website = requests.get(website_url)
-    html_string = html_website.text
-    return html_string
+    path = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path)
+    pdfkit.from_url(website_url, r'.\exports\out.pdf', configuration=config)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 # Press the green button in the gutter to run the script.
 import crawler
 
 if __name__ == '__main__':
-    crawler.get_html_from_url("https://www.google.de/?hl=de")
+    html_str = crawler.get_pdf_from_url("https://meet7.hwr-berlin.de/bigbluebutton/presentation/f7a5e4852ba06fd56f0e875ec62b3111eb631c17-1629902261234/f7a5e4852ba06fd56f0e875ec62b3111eb631c17-1629902261234/2640fb257cb584312c6fd78be225bdbc55afc931-1629903274033/svg/7")
