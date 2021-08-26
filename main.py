efa902fc22cf241e15a6f896f72ac19a9f71b508
Malegr0 <schoening.andreas@gmx.de>
2021-08-26 14:31:34 +0200

    added server.py

--- main.py
+++ main.py
@@ -5,4 +5,8 @@
 import crawler
 
 if __name__ == '__main__':
-    html_str = crawler.get_pdf_from_url("https://meet7.hwr-berlin.de/bigbluebutton/presentation/f7a5e4852ba06fd56f0e875ec62b3111eb631c17-1629902261234/f7a5e4852ba06fd56f0e875ec62b3111eb631c17-1629902261234/2640fb257cb584312c6fd78be225bdbc55afc931-1629903274033/svg/7")
+
+    #html_str = crawler.get_pdf_from_url("https://meet7.hwr-berlin.de/bigbluebutton/presentation"
+    #                                    "/f7a5e4852ba06fd56f0e875ec62b3111eb631c17-1629902261234"
+    #                                    "/f7a5e4852ba06fd56f0e875ec62b3111eb631c17-1629902261234"
+    #                                   "/2640fb257cb584312c6fd78be225bdbc55afc931-1629903274033/svg/7")
